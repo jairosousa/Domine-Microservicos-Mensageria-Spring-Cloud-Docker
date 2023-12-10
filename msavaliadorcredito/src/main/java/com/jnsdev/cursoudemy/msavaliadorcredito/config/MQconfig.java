de2949399de2ac9fe7627fe7139848918a0f9fbe
Jairo Nascimento <jaironsousa@gmail.com>
2023-12-10 16:00:11 -0300

    :rocket: Domine Microservicos e Mensageria com Spring Cloud e Docker  - Correção de error na classe MQconfig.java  - Testando todo o fluxo com o serviço de mensageria.

--- msavaliadorcredito/src/main/java/com/jnsdev/cursoudemy/msavaliadorcredito/config/MQconfig.java
+++ msavaliadorcredito/src/main/java/com/jnsdev/cursoudemy/msavaliadorcredito/config/MQconfig.java
@@ -2,6 +2,7 @@ package com.jnsdev.cursoudemy.msavaliadorcredito.config;
 
 import org.springframework.amqp.core.Queue;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -12,9 +13,10 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MQconfig {
 
-    @Value("${mw.queues.emissao-cartoes}")
+    @Value("${mq.queues.emissao-cartoes}")
     private String emissaoCartoesFila;
 
+    @Bean
     public Queue queueEmissaoCartoes() {
         return new Queue(emissaoCartoesFila, true);
     }
