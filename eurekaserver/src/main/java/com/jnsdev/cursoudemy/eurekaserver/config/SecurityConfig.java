03ea34360e6665cca886883686fbeca169db71a7
Jairo Nascimento <jaironsousa@gmail.com>
2023-12-10 19:41:37 -0300

    :rocket: Domine Microservicos e Mensageria com Spring Cloud e Docker  - Configurando os microservicos para se autenticar com o Eureka

--- eurekaserver/src/main/java/com/jnsdev/cursoudemy/eurekaserver/config/SecurityConfig.java
+++ eurekaserver/src/main/java/com/jnsdev/cursoudemy/eurekaserver/config/SecurityConfig.java
@@ -1,10 +1,10 @@
 package com.jnsdev.cursoudemy.eurekaserver.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -18,11 +18,12 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
+                .csrf(csrf -> csrf.disable())
+                .httpBasic(Customizer.withDefaults())
                 .authorizeHttpRequests((authz) -> authz
                         .anyRequest().authenticated()
-                )
-                .httpBasic(Customizer.withDefaults());
+                );
         return http.build();
     }
+
 }
